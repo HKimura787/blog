32144be77d6f0e24ae75119c3c0bee69c5ba3176
HKimura787 <47412823+HKimura787@users.noreply.github.com>
2024-03-22 12:08:44 +0900

    24-03-22

--- Qiita/win_python_env/install_python.ps1
+++ Qiita/win_python_env/install_python.ps1
@@ -4,19 +4,23 @@ $is_python = Get-Command python -ErrorAction SilentlyContinue
 if ($is_python -eq $null) {
     echo "Python is not installed. Installing Python..."
     # すべてのユーザーにインストールしない、パスを追加する、テストを含めない
-    ./python-3.11.0-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
+    ./python-3.11.0-amd64.exe /quiet InstallAllUsers=0 PrependPath=0 Include_test=0
     echo "Python has been installed."
 }else{
     echo "Python is already installed."
 }
 
 # pythonのパス
-$PYTHON = '\AppData\Local\Programs\Python\Python311\python.exe'
+$PYTHON_RELATIVE = '\AppData\Local\Programs\Python\Python311'
+# 相対パスを絶対パスに変換する
+$PYTHON_DIR = $HOME + $PYTHON_RELATIVE
 
-# 現在のディレクトリを表示する
-echo "Current directory: $PWD"
+# PythonのPathを通す
+echo "Python path: $PYTHON_DIR"
+Set-Item Env:Path $Env:Path";"$PYTHON_DIR
+[Environment]::SetEnvironmentVariable("Path", $Env:Path, [System.EnvironmentVariableTarget]::Machine)
 
 #  NumPyをインストールする
 echo "Installing NumPy..."
 # echo '$python_path'
-& $HOME$PYTHON
+python -m pip install numpy
