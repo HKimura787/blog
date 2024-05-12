54f0873a193876a113122570e324b91e635c772c
HKimura787 <47412823+HKimura787@users.noreply.github.com>
2024-05-13 06:42:57 +0900

    samplingクラス暫定版

---
+++ Qiita/SECR/sampling.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.spatial import distance
+from typing import List, Tuple
+
+class sample:
+    """
+    設定したサンプリング座標の一定の範囲内の点を取得するクラス
+    """
+    def __init__(self, min_x:float, max_x:float, step_x:float, min_y:float, max_y:float, step_y:float, range:float) -> None:
+        self.min_x = min_x
+        self.max_x = max_x
+        self.step_x = step_x
+        self.min_y = min_y
+        self.max_y = max_y
+        self.step_y = step_y
+        self.x_points = np.arange(min_x, max_x, step_x)
+        self.y_points = np.arange(min_y, max_y, step_y)
+        self.range = range
+        return None
+    
+    def get_sample(self, x_list:List[float], y_list:List[float]) -> Tuple[List[float], List[float]]:
+        """
+        サンプリング座標の一定の範囲内の点を取得
+        
+        Args:
+            x_list (List[float]): x座標
+            y_list (List[float]): y座標
+
+        Returns:
+            Tuple[List[float], List[float]]: サンプリング座標の一定の範囲内の点
+        """
+        sample_x = []
+        sample_y = []
+        for x in x_list:
+            for y in y_list:
+                for x_point in self.x_points:
+                    for y_point in self.y_points:
+                        if distance.euclidean([x, y], [x_point, y_point]) <= self.range:
+                            # print(x, y, x_point, y_point)
+                            sample_x.append(x)
+                            sample_y.append(y)
+        return sample_x, sample_y
+
+    def plot_sampling_point(self, ax:plt.subplot=None, color='black', size:int=5) -> None:
+        """
+        サンプリング座標をプロット
+        
+        Args:
+            ax (plt.subplot): サブプロット
+            color (str): プロットの色
+            size (int): プロットのサイズ
+        """
+        if ax is None:
+            fig, ax = plt.subplots()
+        for x in self.x_points:
+            for y in self.y_points:
+                ax.scatter(x, y, color=color, s=size)
+        return None
